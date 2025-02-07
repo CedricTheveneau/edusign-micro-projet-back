bd71efe8a540cc521d762d2fdd2a22060f8ee347
CedricTheveneau <theveneaucedric@gmail.com>
2025-02-07 16:53:51 +0100

    Update jest.config.js
     - [X] Issue seemed to be linked to wrong pattern matching. AN explicit pattern has been declared in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 module.exports = {
- verbose: true,
- testEnvironment: 'node',
- transform: {
-   '^.+\\.js$': 'babel-jest',
- },
-};
+  verbose: true,
+  testEnvironment: 'node',
+  testMatch: [
+   '**/tests/**/*.[jt]s?(x)',
+   '**/?(*.)+(spec|test).[tj]s?(x)'
+ ],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+  },
+ };
